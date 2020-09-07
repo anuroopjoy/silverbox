7ca154afe0cbe4c6862b6a919156033ba1fe8e9c
Anuroop Joy <anuroop.joy@hrblock.com>
2020-09-07 13:13:00 +0530

    land

--- src/app/features/landing.module.ts
+++ src/app/features/landing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { LandingComponent } from './landing/landing.component';
 import { MailboxComponent } from './mailbox/mailbox.component';
 import { MailfolderComponent } from './mailfolder/mailfolder.component';
 
 @NgModule({
     declarations: [
+        LandingComponent,
         MailboxComponent,
         MailfolderComponent
     ],
     imports: [
         RouterModule.forChild([{
-            path: '', component: MailboxComponent
+            path: '', component: LandingComponent
         }])
     ],
     providers: []
