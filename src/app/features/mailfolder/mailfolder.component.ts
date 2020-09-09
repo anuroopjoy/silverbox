87e5486d3c423bfddcf554d7bccb4c020d4a62a0
Arun Jose <arun.jk@outlook.com>
2020-09-09 09:58:48 +0530

    pagination changes partial

--- src/app/features/mailfolder/mailfolder.component.ts
+++ src/app/features/mailfolder/mailfolder.component.ts
@@ -7,18 +7,6 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class MailfolderComponent implements OnInit {
 
-    public items: any = [];
-
-    public itemsToDisplay: any = [];
-
-    private fromArr = [
-        'anuroop.joy@hrblock.com',
-        'kiran.cherian@hrblock.com',
-        'ajith.kumar@hrblock.com',
-        'abhilash.pillai@hrblock.com',
-        'arun.jose@hrblock.com',
-    ];
-
     private attachmentsArr = [
         'Sample.txt',
         'Final.rar',
@@ -30,15 +18,75 @@ export class MailfolderComponent implements OnInit {
         'Finance.doc',
         'Test.7z',
     ];
+    private fromArr = [
+        'anuroop.joy@hrblock.com',
+        'kiran.cherian@hrblock.com',
+        'ajith.kumar@hrblock.com',
+        'abhilash.pillai@hrblock.com',
+        'arun.jose@hrblock.com',
+    ];
 
-    @Input() selectedFolder: number;
-
-    @Output() toggleClicked = new EventEmitter();
+    public items: any = [];
+    public itemsToDisplay: any = [];
+    public pagination = {
+        selected: null,
+        total: null,
+        countToDisplay: null,
+    };
+    public paginationArr = [];
+    public paginationStartIndex = 0;
+    @Input() public selectedFolder: number;
+    @Output() public toggleClicked = new EventEmitter();
 
     constructor() { }
 
-    private randomNumber(min: number, max: number) {
-        return Math.floor(Math.random() * (max - min) + min);
+    public changePage(pageNo: number) {
+        const end = 5 * pageNo;
+        this.itemsToDisplay = this.items.slice(end - 5, end);
+        this.pagination = {
+            selected: pageNo,
+            total: this.items.length,
+            countToDisplay: this.items.length < 5 ? this.items.length : 5,
+        };
+    }
+
+    public ngOnInit(): void {
+        this.createMails();
+        this.itemsToDisplay = this.items.slice(0, 5);
+        this.pagination = {
+            selected: 1,
+            total: this.items.length,
+            countToDisplay: this.items.length < 5 ? this.items.length : 5,
+        };
+        this.setPaginationStart();
+    }
+
+    public setPaginationStart(start: number = 0) {
+        if (start >= 0) {
+            this.paginationStartIndex = start;
+            this.paginationArr = Array(this.pagination.countToDisplay)
+                .fill(null)
+                .map((x, i) => i + start);
+        }
+    }
+
+    private createMails() {
+        for (let i = 0; i < 50; i++) {
+            const date = this.randomDate(
+                new Date(2020, 0, 1),
+                new Date()
+            ).toLocaleDateString();
+            this.items.push({
+                from: this.fromArr[this.randomNumber(0, 5)],
+                subject: '(GT2) Daily Funding Report - ' + date,
+                date,
+                collapse: true,
+                attachments: [
+                    this.attachmentsArr[this.randomNumber(0, 8)],
+                    this.attachmentsArr[this.randomNumber(0, 8)],
+                ],
+            });
+        }
     }
 
     private randomDate(start: Date, end: Date) {
@@ -47,24 +95,8 @@ export class MailfolderComponent implements OnInit {
         );
     }
 
-    private createMails() {
-        for (let i = 0; i < 50; i++) {
-            const date = this.randomDate(new Date(2012, 0, 1), new Date()).toLocaleDateString();
-            this.items.push({
-                from: this.fromArr[this.randomNumber(0, 5)],
-                subject: '(GT2) Daily Funding Report - ' + date,
-                date,
-                collapse: true,
-                attachments: [
-                    this.attachmentsArr[this.randomNumber(0, 8)],
-                    this.attachmentsArr[this.randomNumber(0, 8)]
-                ],
-            });
-        }
+    private randomNumber(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min) + min);
     }
 
-    ngOnInit(): void {
-        this.createMails();
-        this.itemsToDisplay = this.items.slice(0, 5);
-    }
 }
